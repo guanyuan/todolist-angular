78418565b6c241fb3f2c68139d0e87cd615dfb4e
guan yuan <yuanguan89@gmail.com>
2014-10-15 17:20:09 +0800

    add gulp task to start the server and complie the scss file

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass')
+var sass = require('gulp-sass');
+var serve = require('gulp-serve');
+var concat = require('gulp-concat');
+var cssbeautify = require('gulp-cssbeautify');
 
 gulp.task('sass', function () {
-    gulp.src('./app/sass/*.scss')
+    gulp.src('./app/sass/main.scss')
         .pipe(sass())
+        .pipe(cssbeautify())
         .pipe(gulp.dest('./app/css'));
 });
+
+gulp.task('serve', serve('app'));
 
 gulp.task('watch', function() {
   gulp.watch('./app/sass/*.scss', ['sass']);
 });
 
-gulp.task('default',['watch']);
+
+gulp.task('default',['serve', 'sass', 'watch']);
