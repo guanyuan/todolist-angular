5a74d87849b8db3983816a8fc46984b91a58b7b2
guan yuan <yuanguan89@gmail.com>
2014-10-23 16:56:27 +0800

    use angular directive to implement draggable div

--- src/angular-app/directives.js
+++ src/angular-app/directives.js
@@ -1,3 +1,57 @@
 'use strict';
 
 /* Directives */
+
+blogApp.directive('draggableVertical', function() {
+    var linkFn;
+    linkFn = function(scope, element, attrs) {
+        var drag, box;
+        $(element).draggable({axis: "y"});
+    };
+    return {
+    	restrict: 'ACE',
+    	link: linkFn
+    };
+});
+
+
+blogApp.directive('draggableHorizon', function() {
+    var linkFn;
+    linkFn = function(scope, element, attrs) {
+        var drag, box;
+        $(element).draggable({axis: "x"});
+    };
+    return {
+        restrict: 'ACE',
+        link: linkFn
+    };
+});
+
+
+blogApp.directive('myWidget', function() {
+    var linkFn;
+    linkFn = function(scope, element, attrs) {
+        var animateDown, animateRight, pageOne, pageTwo;
+        pageOne = angular.element(element.children()[0]);
+        pageTwo = angular.element(element.children()[1]);
+
+        animateDown = function() {
+            $(this).animate({
+                top: '+=50'
+            });
+        };
+
+        animateRight = function() {
+            $(this).animate({
+                left: '+=50'
+            });
+        };
+
+        $(pageOne).on('click', animateDown);
+        $(pageTwo).on('click', animateRight);
+    };
+    return {
+        restrict: 'E',
+        link: linkFn
+    };
+});
--- src/assets/js/partials/exercise.js
+++ src/assets/js/partials/exercise.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
-	$(".demo-ng-directive .left-side .box").draggable({axis: "y"});
-	$(".demo-ng-directive .right-side .box").draggable({axis: "x"});
+
+	//元素不存在于index.html中所以失效
+    $(".demo-ng-directive .left-side .box").draggable({
+        axis: "y"
+    });
+    $(".demo-ng-directive .right-side .box").draggable({
+        axis: "x"
+    });
+
+    //通过on来绑定未来出现元素的点击事件，可行！然而将body换成#hello，将失效，这是为何呢？
+    $("body").on('click', "#one", function() {
+        $(this).animate({
+            top: '+=50'
+        });
+    });
 });
