d5e495f1081b9ab59f966f5da0ac9562aa813d74
guan yuan <yuanguan89@gmail.com>
2014-10-27 14:53:21 +0800

    make mine-sweeper to be an angular template and use directive to that template

--- src/angular-app/directives.js
+++ src/angular-app/directives.js
@@ -39,4 +39,61 @@ blogApp.directive('draggableHorizon', function() {
     };
 });
 
+/////////////////////////////////////////////////////////////////////////////////////
+blogApp.directive('mineGame', function() {
+    var linkFn;
+    linkFn = function(scope, element, attrs) {
+                var dim = 6;
+        var count = 10;
+        var delay = 500;
 
+
+        generateBoxHtml(element, dim);
+        var bombLocations = generateBombLocation(dim, count);
+        var mineDistArray = getMineDistr(dim, bombLocations);
+        console.log("Bombs located at: " + bombLocations + '(location begins from 0)');
+
+        //布雷及生成格子周边雷数
+        $.each(mineDistArray, function(k, v) {
+            var selector = '.box:nth-child(' + (k + 1) + ') div';
+            if ($.inArray(k, bombLocations) !== -1) {
+                $(selector).addClass("icon-bomb");
+            } else {
+                if (v !== 0) {
+                    $(selector).html(v);
+                }
+            }
+        });
+
+        $("body").on("click", '.box button', function() {
+            var axis = $(this).parent().attr("axis").split('-');
+            axis = {"x": parseInt(axis[0]), "y": parseInt(axis[1])};
+
+            if ($(this).next().hasClass("icon-bomb")) {
+                $(".box button").hide();
+                $(".box div").show();
+                setTimeout(function() {
+                    alert("Do not be sad, you can do better next time.");
+                }, delay);
+            } 
+            else {
+                var indexes = getCurrentSurroundIndex(dim, axis, bombLocations);
+                $.each(indexes, function(k, v) {
+                    $(".box:nth-child(" + (v + 1) + ") button").hide();
+                    $(".box:nth-child(" + (v + 1) + ") div").show();
+                });
+                if ($(".box div:hidden").parent().size() === count) {
+                    setTimeout(function() {
+                        alert("Congratulations, you win!");
+                    }, delay);
+
+                }
+            }
+        });
+        
+    };
+    return {
+        restrict: 'C',
+        link: linkFn
+    };
+});
--- src/assets/js/partials/mine-sweeper.js
+++ src/assets/js/partials/mine-sweeper.js
@@ -1,57 +1,3 @@
-;
-(function($) {
-    $(function() {
-        var dim = 6;
-        var count = 10;
-        var delay = 500;
-
-
-        generateBoxHtml(".mine-layer", dim);
-        var bombLocations = generateBombLocation(dim, count);
-        var mineDistArray = getMineDistr(dim, bombLocations);
-        console.log("Bombs located at: " + bombLocations + '(location begins from 0)');
-
-        //布雷及生成格子周边雷数
-        $.each(mineDistArray, function(k, v) {
-            var selector = '.box:nth-child(' + (k + 1) + ') div';
-            if ($.inArray(k, bombLocations) !== -1) {
-                $(selector).addClass("icon-bomb");
-            } else {
-                if (v !== 0) {
-                    $(selector).html(v);
-                }
-            }
-        });
-
-        $("body").on("click", '.box button', function() {
-            var axis = $(this).parent().attr("axis").split('-');
-            axis = {"x": parseInt(axis[0]), "y": parseInt(axis[1])};
-
-            if ($(this).next().hasClass("icon-bomb")) {
-                $(".box button").hide();
-                $(".box div").show();
-                setTimeout(function() {
-                    alert("Do not be sad, you can do better next time.");
-                }, delay);
-            } 
-            else {
-                var indexes = getCurrentSurroundIndex(dim, axis, bombLocations);
-                $.each(indexes, function(k, v) {
-                    $(".box:nth-child(" + (v + 1) + ") button").hide();
-                    $(".box:nth-child(" + (v + 1) + ") div").show();
-                });
-                if ($(".box div:hidden").parent().size() === count) {
-                    setTimeout(function() {
-                        alert("Congratulations, you win!");
-                    }, delay);
-
-                }
-            }
-
-        });
-    });
-
-
     //洗牌后选择前count个元素，随机选择布雷位置
     function generateBombLocation(dim, count) {
         var array = [];
@@ -154,16 +100,14 @@
 
         for (var i = 0; i < dim; i++) {
             for (var j = 0; j < dim; j++) {
-                $(selector).append(boxHtmlCode);
-                $(selector + " .box").last().attr("axis", i + "-" + j);
+                selector.append(boxHtmlCode);
+                $('.' + selector.attr("class").split(' ')[0] + ' .box').last().attr("axis", i + "-" + j);
             };
         };
-        var width = $(selector).children().width();
-        $(selector).children().css({
-            "height": width,
-            "font-size": (width / 2),
-            "line-height": width + "px"
+        var width = $(selector.children().eq(0)).width();
+        selector.children().css({
+            "height": width+"px",
+            "font-size": (width / 2)+"px",
+            "line-height": width+"px"
         });
     }
-
-})(window.jQuery);
